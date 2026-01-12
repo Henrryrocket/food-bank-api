3b2a5a361755bb5d84165cca3e5476da8eb5d9d8
henrryrocket <henrryrocket@gmail.com>
2026-01-12 20:59:30 -0300

    Add model

---
+++ src/models/inventoryModel.js
@@ -0,0 +1,26 @@
+//  DB
+let database = [
+  { id: 1, name: 'Arroz', quantity: 50, unit: 'kg' },
+  { id: 2, name: 'Leche', quantity: 20, unit: 'litros' },
+];
+
+export const inventoryModel = {
+  findAll: async () => {
+    return database;
+  },
+  findById: async (id) => {
+    return database.find((item) => item.id === id);
+  },
+
+  create: async (item) => {
+    const newItem = { id: database.length + 1, ...item };
+    database.push(newItem);
+    return newItem;
+  },
+
+  delete: async (id) => {
+    const initialLength = database.length;
+    database = database.filter((item) => item.id !== id);
+    return database.length < initialLength; // Retorna true si borró algo
+  },
+};
